3b88f654cd43a0e7491615b85a77f138a98b7695
gluonZeta <andrainat@gmail.com>
2024-03-19 07:33:45 +0100

    Config logBack

--- controller/src/main/java/gluon/projects/SchoolProjectMain.java
+++ controller/src/main/java/gluon/projects/SchoolProjectMain.java
@@ -1,5 +1,7 @@
 package gluon.projects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,11 +10,14 @@ import java.util.Arrays;
 
 @SpringBootApplication
 public class SchoolProjectMain {
+
+    private static final Logger logger = LoggerFactory.getLogger(SchoolProjectMain.class);
+
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SchoolProjectMain.class, args);
-        System.out.println("------------------------------------------------------------------------------------");
+        logger.info("------------------------------------------------------------------------------------");
         String[] beanNames = context.getBeanDefinitionNames();
-        Arrays.stream(beanNames).filter(x -> !x.contains("org.springframework")).forEach(System.out::println);
-        System.out.println("------------------------------------------------------------------------------------");
+        Arrays.stream(beanNames).filter(x -> !x.contains("org.springframework")).forEach(logger::info);
+        logger.info("------------------------------------------------------------------------------------");
     }
 }
